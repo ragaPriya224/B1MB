2406a409ea431fe89757e40adeaa5dfeb19edd1d
bbb <aaa@gmail.com>
2022-07-28 08:10:02 +0530

    refs#sample

---
+++ springboot-search-rest-api-demo/src/main/java/com/practice/springbootsearchrestapidemo/SpringbootSearchRestApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springbootsearchrestapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSearchRestApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSearchRestApiDemoApplication.class, args);
+	}
+
+}
